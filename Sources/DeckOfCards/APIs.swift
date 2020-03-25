651c0621a4d8331ce47ceca5d594ccc08f7380cf
Roderic Campbell <roderic@gmail.com>
2020-03-24 23:23:05 -0700

    Update basePath

--- Sources/DeckOfCards/APIs.swift
+++ Sources/DeckOfCards/APIs.swift
@@ -7,7 +7,7 @@
 import Foundation
 
 open class SwaggerClientAPI {
-    public static var basePath = "https://virtserver.swaggerhub.com/Thumbworks/DeckOfCards/1.0.0"
+    public static var basePath = "https://deckofcardsapi.com/api"
     public static var credential: URLCredential?
     public static var customHeaders: [String:String] = [:]
     public static var requestBuilderFactory: RequestBuilderFactory = AlamofireRequestBuilderFactory()
